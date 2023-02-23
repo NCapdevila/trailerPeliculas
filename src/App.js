cca4be10a8a8619a7387a1c8b1120b6ea24db2af
Ramiro Capdevila <ramirocapdevila23@gmail.com>
2023-02-23 20:34:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Youtube from 'react-youtube'
 import './App.css';
 
 function App() {
+    const API_URL = 'https://api.themoviedb.org/3'
+    const API_KEY = '390a2a575f691440ebbaef0c93bb0baf'
+    const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+    const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
+
+  //variables de estado
+
+  const [movies, setMovies] = useState([])
+  const [searchKey, setSearchKey] = useState("")
+  const [trailer, setTrailer] = useState(null)
+  const [movie, setMovie] = useState({title: "Loading Movies"})
+  const [paying, setPlaying] = useState(false)
+
+  //función para realizar la petición a la api
+
+  const fetchMovies = async (searchKey) =>{
+    const type = searchKey ? "search" : "discover"
+    const {data: {results}} = await axios.get(`${API_URL}/${type}/movie`,{
+    params:{
+      api_key: API_KEY,
+      query: searchKey
+    },
+  });
+
+  setMovies(results)
+  setMovie(results[0])
+
+
+  }
+
+  useEffect(()=>{
+    fetchMovies();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container mt-3'>
+        <div className='row'>
+          {movies.map((movie)=>( 
+            <div key={movie.id} className="col-md-4 mb-3">
+              <img src={`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width="100%"/>
+              <h4 className='text-center'>{movie.title}</h4>
+            </div> 
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
